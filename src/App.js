3e660fa2977c829859526acb92e222a6b38e1e97
Gunlayer <mr.mcdrive@gmail.com>
2021-09-23 16:15:42 +0300

    style complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Currensy from './Currensy.json';
 import './App.css';
 
 
-function App() {  
+function App() {
   const currencyArr = Currensy.exchangeRates;
   const [value, setvalue] = useState("");
 
   const [inputSelectState, setinputSelectState] = useState(currencyArr[0].code)
   const [outputSelectState, setoutputSelectState] = useState(currencyArr[0].code);
-  
+
   const calcResult = () => {
     let result = value / getCurrentSelectedRate(inputSelectState) * getCurrentSelectedRate(outputSelectState);
     return result.toFixed(2);
@@ -20,32 +20,36 @@ function App() {
     return currentSelectedValue[0].rate;
   }
   const swapCurrency = () => {
-    
+
     const tmp = inputSelectState;
     inputSelectState = outputSelectState;
     outputSelectState = tmp;
   }
-  
-  
-  
+
+
+
 
   return (
     <div className="container">
       <h1>Currency Converter</h1>
-      <form>
-        <select id="in" className="selector" value={inputSelectState} onChange={event=>setinputSelectState(event.target.value)}>
-          {Currensy.exchangeRates.map(item => (<option value={item.code} data-rate={item.rate}>{item.code}</option>))}  
-        </select>
-        <input type="text" maxLength="6" defaultValue="0" className="input-form" id="inputValue" onChange={event=>setvalue(event.target.value)}></input>
+      <div className="input-box">
+        <form>
+          <select id="in" className="selector" value={inputSelectState} onChange={event => setinputSelectState(event.target.value)}>
+            {Currensy.exchangeRates.map(item => (<option value={item.code} data-rate={item.rate}>{item.code}</option>))}
+          </select>
+          <input type="text" maxLength="6" defaultValue="0" className="input-form" id="inputValue" onChange={event => setvalue(event.target.value)}></input>
+        </form>
+      </div>
 
-        <button className="btn" onClick={swapCurrency}>swap</button>
-
-        <select id="out" className="selector" value={outputSelectState} onChange={event => setoutputSelectState(event.target.value)}>
-          {Currensy.exchangeRates.map(item => (<option value={item.code} data-rate={item.rate}>{item.code}</option>))}  
-        </select>
-        <input type="text" maxLength="6" value={calcResult()} className="input-form" id="outputValue" disabled></input>
-      </form>
-      
+      <button className="btn" onClick={swapCurrency}>swap</button>
+      <div className="input-box">
+        <form>
+          <select id="out" className="selector" value={outputSelectState} onChange={event => setoutputSelectState(event.target.value)}>
+            {Currensy.exchangeRates.map(item => (<option value={item.code} data-rate={item.rate}>{item.code}</option>))}
+          </select>
+          <input type="text" maxLength="6" value={calcResult()} className="input-form" id="outputValue" disabled></input>
+        </form>
+      </div>
     </div>
   );
 }
